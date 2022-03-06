49a653c74714c49fd8f33860a7b996b85c2612bf
Yororing <gksap88@gmail.com>
2022-03-06 18:59:47 +0900

    Practicing Express

--- src/server.js
+++ src/server.js
@@ -1,3 +1,24 @@
+//Call express from Module
 import express from "express";
 
-console.log("HI Im good");
+const PORT = 4000;
+//Create express Application
+const app = express();
+
+//How to Using Express
+const handleAccess = (req, res) => {
+    return res.send("<h1>Access will be Done</h1>");
+};
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+};
+
+//Get Request And Callback handleAccess
+app.get("/", handleAccess);
+app.get("/login", handleLogin);
+
+const handleListening = () => 
+    console.log(`Server listening on port http://localhost:${PORT}`);
+
+//Make CallBack on Port 4000
+app.listen(PORT, handleListening);
